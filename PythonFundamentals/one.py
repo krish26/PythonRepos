4d2123a55e95d24d18a61b48dd2a1f3768183a44
krishna koumudi <koumudi26@gmail.com>
2025-12-09 11:03:36 +0100

    first commit

---
+++ PythonFundamentals/one.py
@@ -0,0 +1,27 @@
+"""""
+my_list=['koumudi',20,100.00,'o',89]
+my_list =my_list+ ['new_item']
+print(my_list *9)
+
+l=[1,2,3]
+l.append("append me !")
+popped_item=l.pop(0)
+print(popped_item)
+print(l[100])
+
+new_list=['a','e','x']
+new_list.sort()
+print(new_list)
+"""
+
+lst_1=[1,2,3]
+lst_2=[4,5,6]
+lst_3=[7,8,9]
+
+matrix = [lst_1,lst_2,lst_3]
+print(matrix[0][0])
+
+first_col = [row[0] for row in matrix]
+print(first_col)
+
+
